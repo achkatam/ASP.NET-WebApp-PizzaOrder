2b273a56f0b79ead8396effdcfccd2147bb7c81e
achkatam <agmatevv@gmail.com>
2022-12-26 16:38:47 -0500

    Refactored text alignment left to center in Checkout. Also the container was centered as well

--- Pages/Checkout/ThankYou.cshtml.cs
+++ Pages/Checkout/ThankYou.cshtml.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyPizzeria.Data;
+using MyPizzeria.Models;
 
 namespace MyPizzeria.Pages.Checkout
 {
     public class ThankYouModel : PageModel
     {
+      
         public void OnGet()
         {
+         
         }
     }
 }
